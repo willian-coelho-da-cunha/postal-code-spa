bd67eb4235c8247f717ab879fe1557a91cfc02dc
Willian Coelho da Cunha <willian_comp@outlook.com>
2020-08-28 01:09:19 -0300

    Implement email-form-field component.

--- src/app/library/email-form-field/email-form-field.module.ts
+++ src/app/library/email-form-field/email-form-field.module.ts
@@ -1,13 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
 import { EmailFormFieldComponent } from './email-form-field/email-form-field.component';
 
-
-
 @NgModule({
-  declarations: [EmailFormFieldComponent],
+  declarations: [
+    EmailFormFieldComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatFormFieldModule
   ]
 })
 export class EmailFormFieldModule { }
--- src/app/library/email-form-field/email-form-field/email-form-field.component.ts
+++ src/app/library/email-form-field/email-form-field/email-form-field.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnChanges, Input, SimpleChanges } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-email-form-field',
   templateUrl: './email-form-field.component.html',
   styleUrls: ['./email-form-field.component.css']
 })
-export class EmailFormFieldComponent implements OnInit {
+export class EmailFormFieldComponent implements OnChanges {
 
-  constructor() { }
+  public emailFormField = new FormControl('', [ Validators.email ]);
 
-  ngOnInit(): void {
+  @Input() public ipLabel = '';
+
+  @Input() public ipRequired = '';
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes && changes['ipRequired'] && changes['ipRequired'].previousValue !== changes['ipRequired'].currentValue) {
+      if (changes['ipRequired'].currentValue === true) {
+        this.emailFormField.setValidators([ Validators.email, Validators.required ]);
+      } else {
+        this.emailFormField.setValidators([ Validators.email ]);
+      }
+      this.emailFormField.updateValueAndValidity();
+    }
   }
 
+  public getValue(): string {
+    return this.emailFormField.value;
+  }
 }

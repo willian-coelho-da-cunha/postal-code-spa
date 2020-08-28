4b3336f8ed933952405c28e587ba784915601db8
Willian Coelho da Cunha <willian_comp@outlook.com>
2020-08-28 01:31:32 -0300

    Create cancel-button module.

---
+++ src/app/library/cancel-button/cancel-button.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CancelButtonComponent } from './cancel-button/cancel-button.component';
+
+
+
+@NgModule({
+  declarations: [CancelButtonComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class CancelButtonModule { }

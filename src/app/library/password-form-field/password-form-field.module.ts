691e3066453984c24c3a6bd7a05058efa55993be
Willian Coelho da Cunha <willian_comp@outlook.com>
2020-08-28 00:34:27 -0300

    Create password-form-field module.

---
+++ src/app/library/password-form-field/password-form-field.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PasswordFormFieldComponent } from './password-form-field/password-form-field.component';
+
+
+
+@NgModule({
+  declarations: [PasswordFormFieldComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class PasswordFormFieldModule { }
---
+++ src/app/library/password-form-field/password-form-field/password-form-field.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-password-form-field',
+  templateUrl: './password-form-field.component.html',
+  styleUrls: ['./password-form-field.component.css']
+})
+export class PasswordFormFieldComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

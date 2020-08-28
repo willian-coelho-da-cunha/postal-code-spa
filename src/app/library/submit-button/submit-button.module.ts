a2b54030ac38d7fc28c508aece729b92c5666d60
Willian Coelho da Cunha <willian_comp@outlook.com>
2020-08-28 01:20:51 -0300

    Create submit-button module.

---
+++ src/app/library/submit-button/submit-button.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SubmitButtonComponent } from './submit-button/submit-button.component';
+
+
+
+@NgModule({
+  declarations: [SubmitButtonComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class SubmitButtonModule { }

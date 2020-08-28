fe6783e4e8e6adf2cca23b4b625d5efdc6f62d8d
Willian Coelho da Cunha <willian_comp@outlook.com>
2020-08-28 00:46:04 -0300

    Implement password-form-field component.

--- src/app/library/password-form-field/password-form-field.module.ts
+++ src/app/library/password-form-field/password-form-field.module.ts
@@ -1,13 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
 import { PasswordFormFieldComponent } from './password-form-field/password-form-field.component';
 
-
-
 @NgModule({
-  declarations: [PasswordFormFieldComponent],
+  declarations: [
+    PasswordFormFieldComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatFormFieldModule
   ]
 })
 export class PasswordFormFieldModule { }
--- src/app/library/password-form-field/password-form-field/password-form-field.component.ts
+++ src/app/library/password-form-field/password-form-field/password-form-field.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-password-form-field',
   templateUrl: './password-form-field.component.html',
   styleUrls: ['./password-form-field.component.css']
 })
-export class PasswordFormFieldComponent implements OnInit {
+export class PasswordFormFieldComponent {
 
-  constructor() { }
+  public passwordFormField = new FormControl('');
 
-  ngOnInit(): void {
+  public passwordHidden = true;
+
+  @Input() public ipLabel = '';
+
+  @Input() public ipRequired = false;
+
+  public getValue(): string {
+    return this.passwordFormField.value;
   }
-
 }

8cf07554336ead5d43f5f8812fcef17fc1e0928f
Willian Coelho da Cunha <willian_comp@outlook.com>
2020-09-13 00:15:08 -0300

    Create container module and component.

---
+++ src/app/library/container/container.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ContainerComponent } from './container/container.component';
+
+
+
+@NgModule({
+  declarations: [ContainerComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class ContainerModule { }

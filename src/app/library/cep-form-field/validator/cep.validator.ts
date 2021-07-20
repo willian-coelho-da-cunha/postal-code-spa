716b399161f1cab7122ad64d90ecbb765e632028
Willian Coelho da Cunha <willian_comp@outlook.com>
2021-07-20 00:32:28 -0300

    Update cep form field validator.

--- src/app/library/cep-form-field/validator/cep.validator.ts
+++ src/app/library/cep-form-field/validator/cep.validator.ts
@@ -2,7 +2,7 @@ import { ValidatorFn, AbstractControl } from '@angular/forms';
 
 export function cepValidator(): ValidatorFn {
   return (control: AbstractControl): { [key: string]: boolean } | null => {
-    if (control.value && (/(\d)\w\1/).test(String(control.value))) {
+    if (!control.value || control.value < 10000000) {
       return { cep: true };
     }
     return null;
